c9916bc825bee770045af0ed81b642a4817963a8
Erison Nicodemos <erisonnicodemos@gmail.com>
2020-04-09 15:10:05 -0300

    update angular version to 9

--- frontend/src/app/shared/home/home.component.ts
+++ frontend/src/app/shared/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Banco } from 'src/app/models/banco.model';
 import { Bloco } from 'src/app/models/bloco.model';
-import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery-9';
 
 
 @Component({
